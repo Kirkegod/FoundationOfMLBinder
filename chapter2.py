61bc7e2de631e3e006227cde8fda843a38a6ce67
joeloskarsson <joel.oskarsson@liu.se>
2021-05-28 12:01:40 +0200

    Set up env for exercise 2.1

---
+++ chapter2.py
@@ -0,0 +1,15 @@
+# Exercise 2.1
+def load_bat_data():
+    train_data = np.loadtxt("data/chapter2/bat_train_data.csv", delimiter=",")
+    test_data = np.loadtxt("data/chapter2/bat_test_data.csv", delimiter=",")
+
+    X_train = train_data[:, :2]
+    y_train = (train_data[:, 2]).astype(int)
+
+    X_test = test_data[:, :2]
+    y_test = (test_data[:, 2]).astype(int)
+
+    return X_train, y_train, X_test, y_test
+
+# Exercise 2.2
+# TODO

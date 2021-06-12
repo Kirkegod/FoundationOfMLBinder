bb5592bf91cd6cb2d0ac27bd95742a815a57e20e
Amanda Olmin <amaol67@liu.se>
2021-06-12 15:17:40 +0200

    Addition of chapter3 helper code

--- chapter3.py
+++ chapter3.py
@@ -18,3 +18,39 @@ def load_california_data():
     y_test = y[n_train:]
 
     return X_train, y_train, X_test, y_test
+    
+def load_penguin_data():
+    """ Load palmer penguin data including flipper length, bill length, body mass (attributes) and species (target)
+    :return:
+        X_train: train observations (n_train, n_attributes)
+        y_train: train targets (n_train,)
+        X_test: test observations (n_test, n_attributes)
+        y_test: test targets (n_test,)
+        classes: vector of available classes (n_classes,)
+    """
+    df = load_penguins()
+    attributes = ["bill_length_mm", "body_mass_g", "flipper_length_mm"]
+    target = "species"
+
+    X = np.array(df[attributes])
+    targets = np.array(df[target])
+
+    # These two observations actually miss all chosen values, so we will exclude them
+    nan_ind = np.isnan(X[:, 0] * X[:, 1] * X[:, 2])
+    X = X[~nan_ind, :]
+    targets = targets[~nan_ind]
+    classes, y = np.unique(targets, return_inverse=True)
+
+    # Split into train and test (randomly)
+    n_train = 274
+
+    np.random.seed(21)
+    shuffled_inds = np.random.choice(X.shape[0], X.shape[0], replace=False)
+
+    train_inds = shuffled_inds[:n_train]
+    test_inds = shuffled_inds[n_train:]
+
+    X_train, y_train = X[train_inds, :], y[train_inds]
+    X_test, y_test = X[test_inds, :], y[test_inds]
+
+    return X_train, y_train, X_test, y_test, classes

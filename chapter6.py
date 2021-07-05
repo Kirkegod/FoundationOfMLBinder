294ec05cc248ec96c634fb892e374238af6e7ff6
Amanda Olmin <amaol67@liu.se>
2021-07-05 16:57:26 +0200

    Addition of helper functions GMM section 6

--- chapter6.py
+++ chapter6.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
 import scipy.stats as sps
 
 def load_labeled_mnist_data():
@@ -25,6 +26,70 @@ def load_full_mnist_data():
     X_test, y_test = test_data[:, :-1], test_data[:, -1]
 
     return X_train_l, y_train_l, X_train_u, X_test, y_test
+    
+def visualize_data(X_l, y_l, X_u=None, ax=None, show=True):
+    """Visualise 2D class data
+    X_l: (n_l, p) labeled input
+    y_l: (n_l,) targets for labeled input
+    X_u: (n_u, p) unlabeled input
+    """
+
+    if ax is None:
+        fig, ax = plt.subplots()
+
+    if X_u is not None:
+        ax.scatter(X_u[:, 0], X_u[:, 1], c='#7f7f7f', alpha=0.5)
+
+    classes = ['6', '7', '8']
+    for i in range(len(classes)):
+        ax.scatter(X_l[y_l == i, 0], X_l[y_l == i, 1])
+
+    if X_u is not None:
+        classes = ['unknown'] + classes
+
+    ax.set_xlabel("PC 1")
+    ax.set_ylabel("PC 2")
+    ax.legend(classes)
+
+    if show:
+        plt.show()
+
+def visualize_clusters(X_train, y_train, mu, sigma, X_train_u=None):
+    """Visualize clusters
+    X_train: (n_train, 2) input array of dimension 2
+    y_train: (n_train,) target array
+    mu: (n_classes, 2) array of estimated means
+    sigma: (n_classes, 2, 2) array of estimated covariance matrices
+    X_train_u: (n_train_u, 2) additional unlabeled data
+    """
+
+    # Plot data
+    fig, ax = plt.subplots()
+
+    visualize_data(X_train, y_train, X_train_u, ax, show=False)
+
+    classes = np.unique(y_train)
+    for i in range(len(classes)):
+        # Plot distribution contour
+        draw_ellipse(mu[i, :], sigma[i, :, :], ax)
+
+    plt.show()
+    
+    
+def draw_ellipse(mu, sigma, ax):
+    """Draw 2D ellipse corresponding to contour of Gaussian distribution with specified mean and covariance (2 stds)
+    mu: (2,) mean vector
+    sigma: (2, 2) covariance matrix
+    ax: plot axis
+    """
+
+    # Convert covariance to principal axes
+    u, s, vh = np.linalg.svd(sigma)
+    angle = float(np.degrees(np.arctan2(u[1, 0], u[0, 0])))
+    width, height = 2 * np.sqrt(s)
+
+    # Draw ellipse
+    ax.add_patch(Ellipse(mu, 2 * width, 2 * height, angle, edgecolor='k', facecolor='none'))
 
 
 def plot_kde(data, ax, ax_lim=None):
